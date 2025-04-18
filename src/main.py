c3fc2489044a23c503967854d1d5e389be5b008b
Jason <jasonssdev@gmail.com>
2025-04-18 16:14:06 -0500

    chains

--- src/main.py
+++ src/main.py
@@ -9,3 +9,30 @@ from pydantic import BaseModel
 api_key = settings['openai']
 model = 'o4-mini-2025-04-16'
 llm = ChatOpenAI(api_key=api_key, model=model)
+
+class Answer(BaseModel):
+    number_of_words: int
+
+# Template
+prompt_template = PromptTemplate.from_template("""
+    I will give a text and you will return the number of words in it.
+    Text: {text}
+""")
+#prompt
+prompt = prompt_template.invoke({
+    'text': 'This is an example with langchain'
+})
+# structured output
+llm_structured = llm.with_structured_output(Answer)
+
+# print
+# llm_result = llm_structured.invoke(prompt)
+# print(llm_result)
+
+# all of that could be resolved with chain
+
+chain = prompt_template | llm_structured
+chain_result = chain.invoke({
+    'text': 'This is an example with langchain'
+})
+print(chain_result)
