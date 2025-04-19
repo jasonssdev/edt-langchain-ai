d31d3b8b71dadda0d489d1c245c14d528e49e5dd
Jason <jasonssdev@gmail.com>
2025-04-19 06:50:23 -0500

    gitignore

--- src/main.py
+++ src/main.py
@@ -8,3 +8,14 @@ from langgraph.graph import StateGraph, START, END
 api_key = settings['openai']
 model = 'o4-mini-2025-04-16'
 llm = ChatOpenAI(api_key=api_key, model=model)
+# from langchain_community.document_loaders import PyPDFLoader
+
+api_key = settings['openai']
+model = 'o4-mini-2025-04-16'
+llm = ChatOpenAI(api_key=api_key, model=model)
+
+print("working")
+
+# loader = PyPDFLoader("./../data/raw/file.pdf")
+# content = loader.load()
+# print(content[0].page_content)
